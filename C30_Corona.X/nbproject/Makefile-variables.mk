700412a229efd834954f37b0bb6c0b494d12aa28
Harpreet Singh <Harpreet.Singh@perkinelmer.com>
2018-08-17 15:20:39 -0400

    Upgraded to MpLab 5

--- C30_Corona.X/nbproject/Makefile-variables.mk
+++ C30_Corona.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=Corona-V19.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/Corona-V19.X.production.hex
+CND_ARTIFACT_NAME_default=C30_Corona.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/C30_Corona.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=corona-v19.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/corona-v19.x.tar
+CND_PACKAGE_NAME_default=c30corona.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/c30corona.x.tar
--- C30_Corona.X/nbproject/Package-default.bash
+++ C30_Corona.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Corona-V19.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Corona-V19.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=corona-v19.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/C30_Corona.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=C30_Corona.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=c30corona.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/corona-v19.x/bin
+makeDirectory ${TMPDIR}/c30corona.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/corona-v19.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/c30corona.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/corona-v19.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/c30corona.x.tar *
 checkReturnCode
 
 # Cleanup
